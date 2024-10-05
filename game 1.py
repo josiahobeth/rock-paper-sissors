d6497adc30f6c4547d27246840aacd8dc834e66e
josiahobeth <120653691+josiahobeth@users.noreply.github.com>
2024-10-05 19:59:40 +0530

    Add files via upload

---
+++ game 1.py
@@ -0,0 +1,35 @@
+import random
+
+b=["rock","paper","sissors"]
+sum=0
+choice=0
+winner="*******Winner********"
+lose="******Better luck next time******"
+while True:
+    a=str(input("enter your choice:"))
+    c=random.choice(b)
+    if a=="rock" and c=="sissors":
+        choice+=1
+    elif a=="paper" and c=="rock":
+        choice+=1
+    elif a=="sissors" and c=="paper":
+        choice+=1
+    elif a=="rock" and c=="rock":
+        choice+=0
+    elif a=="paper" and c=="paper":
+        choice+=0
+    elif a=="sissors" and c=="sissors":
+        choice+=0
+    else:
+        sum+=1
+    print("opponent choice:",c)
+    print("opponent point:->",sum)
+    print("your point:->",choice)
+    if(choice==1):
+        print(winner)
+    break
+    if(sum==1):
+        print(lose)
+    break
+
+
